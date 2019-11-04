f11d2fcdaebe467f76b9eb20d4c45b76cf6b70f0
seniorpine <katsuhiko.yano@gmail.com>
2019-11-04 10:44:03 +0900

    tf2.0 classifier

--- classifier.py
+++ classifier.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 
 # Tensorflow 2.0 Eager execution 
-# mnist
+# mnist / using tf.keras
 
 class Trainer(object):
   def __init__(self, inputs, outputs):
@@ -13,38 +13,37 @@ class Trainer(object):
     self.accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
     self.optimizer = tf.keras.optimizers.Adam()
     self.model_path = './my_model'
-    self.create_model(inputs, outputs)
+    self.build_model(inputs, outputs)
     self.checkpoint_prefix = os.path.join(self.model_path, "ckpt")
     self.ckptroot = tf.train.Checkpoint(optimizer=self.optimizer, model=self.model)
 
-  def create_model(self, inputs, outputs):
+  def build_model(self, inputs, outputs) -> None:
     inputs_ = layers.Input((inputs,))
     x = layers.Dense(128, activation="relu")(inputs_)
     x = layers.Dense(64, activation="relu")(x)
     x = layers.Dense(outputs, activation="softmax")(x)
     self.model = tf.keras.models.Model(inputs_, x)
-    return self.model
 
   @tf.function
-  def train_on_batch(self, X, y):
+  def train_step(self, x, y) -> float:
     with tf.GradientTape() as tape:
-      prediction = self.model(X, training=True)
+      prediction = self.model(x, training=True)
       loss = self.loss(y, prediction)
     graidents = tape.gradient(loss, self.model.trainable_weights)
     self.optimizer.apply_gradients(zip(graidents, self.model.trainable_weights))
     self.accuracy.update_state(y, prediction)
     return loss
 
-  def train(self, trainset, epochs):
+  def train(self, dataset, epochs) -> None:
     for epoch in range(epochs):
       self.accuracy.reset_states()
-      for step, (X, y) in enumerate(trainset):
-        loss_val = self.train_on_batch(X, y)
+      for step, (x, y) in enumerate(dataset):
+        loss = self.train_step(x, y)
         if step % 100 == 0:
-          print("epoch = {} step = {} loss = {} accuracy = {}".format(epoch, step, loss_val, self.accuracy.result()))
+          print("epoch = {} step = {} loss = {} accuracy = {}".format(epoch, step, loss, self.accuracy.result()))
 
-  def save(self):
+  def save(self) -> None:
     self.ckptroot.save(self.checkpoint_prefix)
   
-  def load(self):
+  def load(self) -> None:
     self.ckptroot.restore(tf.train.latest_checkpoint(self.model_path))
